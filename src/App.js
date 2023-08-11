9b5f31abb546f8651c65b4d7700a627eb4819a1f
Yuvraj <kingyuvi88@gmail.com>
2023-08-11 16:44:43 +0530

    Added UI design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 
 function App() {
+
+if('SpeechRecognition' in window || 'webkitSpeechRecognition' in window){
+  const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+
+}
+
   return (
     <>
-    <h1 className="text-red-500">Speech to text</h1>
+
+    <div className="flex flex-col  items-center mt-36 ">
+    <h1 className="text-3xl font-bold mb-10  ">Speech to Text convertor</h1>
+    <textarea className="p-2 border-2 border-black w-[60%] h-48" placeholder="Hello there..."></textarea>
+    <button className="mt-10 bg-green-500 p-2 text-white text-xl rounded-md ">Start Recording</button>
+      
+    </div>
     </>
   );
 }
