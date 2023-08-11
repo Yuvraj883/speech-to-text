aba4799747a60257793c13266762e19cbc5305ef
Yuvraj <kingyuvi88@gmail.com>
2023-08-11 17:13:14 +0530

    Implemented the logic

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
+import React, { useState } from "react";
 
 function App() {
-
-if('SpeechRecognition' in window || 'webkitSpeechRecognition' in window){
+  const [recording, setRecording] = useState(false);
+  const [transcript, setTranscript] = useState("");
   const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+  recognition.lang = 'en-US';
+  recognition.interimResults= true;
 
-}
+  const startRecording = () => {
+    if (recording) {
+      recognition.stop();
+      setRecording(false);
+    } else {
+      recognition.start();
+      setRecording(true);
+    }
+  }
+
+  recognition.onresult = (event) => {
+    const text = event.results[0][0].transcript;
+    setTranscript(text);
+    console.log("Transcript:", event);
+  }
 
   return (
     <>
-
-    <div className="flex flex-col  items-center mt-36 ">
-    <h1 className="text-3xl font-bold mb-10  ">Speech to Text convertor</h1>
-    <textarea className="p-2 border-2 border-black w-[60%] h-48" placeholder="Hello there..."></textarea>
-    <button className="mt-10 bg-green-500 p-2 text-white text-xl rounded-md ">Start Recording</button>
-      
-    </div>
+      <div className="flex flex-col items-center mt-36">
+        <h1 className="text-3xl font-bold mb-10">Speech to Text Converter</h1>
+        <p className="p-2 border-2 border-black w-[60%] h-48">{transcript}</p>
+        <button onClick={startRecording} className="mt-10 bg-green-500 p-2 text-white text-xl rounded-md ">
+          {recording ? 'Recording...' : 'Start Recording'}
+        </button>
+      </div>
     </>
   );
 }
